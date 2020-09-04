7f13f020cabcbdeff0027f272b29374cfbfc171f
sudhisharma <sudhisharma4596@gmaill.com>
2020-09-04 20:12:41 +0530

    added datetime module

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
-print("test")
+# Script to get different in data
+
+from  datetime import date
+
+
+today = date.today()
+
+nyd = date(2022,6,30)
+
+print(today)
+print(nyd)
+
+print("difference in days is " , (nyd-today).days)
