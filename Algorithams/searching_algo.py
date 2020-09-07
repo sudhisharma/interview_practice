97de32a49ac2d2341627ea62a5d74328711c0e25
sudhisharma <sudhisharma4596@gmaill.com>
2020-09-07 19:53:26 +0530

    new search_algarithom

---
+++ Algorithams/searching_algo.py
@@ -0,0 +1,26 @@
+#Search a element in a list and provide the index
+
+list1 = [1,4,8,5,6,0,3]
+
+def search_value(sequence,value):
+    count = 0
+    length = len(sequence) #7
+    while count<length :
+        if sequence[count] == value:
+            break
+        else:
+            count +=1
+
+    if count == length:
+        return None
+    return count
+
+print(search_value(list1,10))
+
+
+def using_inbuild(sequence,value):
+    for i,j in enumerate(sequence):
+        if j == value:
+            return i
+
+print(using_inbuild(list1,10))
