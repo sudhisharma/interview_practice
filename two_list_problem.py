9e4c66859c609e6158187febf4bf289a2fc30193
sudhisharma <sudhisharma4596@gmaill.com>
2020-09-10 13:18:04 +0530

    two lists problem

--- two_list_problem.py
+++ two_list_problem.py
@@ -10,6 +10,15 @@ output :- a1b2b3
 l1 = "abc"
 l2 = "123"
 
+def com_values(list1,list2):
+    if not list1 :
+        return list2
+    elif not list2:
+        return list1
+    else:
+        return list1[i] + list2[i]
+
+
 def combining(list1,list2):
     new_list = []
     for i in range(len(list1)):
@@ -18,3 +27,4 @@ def combining(list1,list2):
     return "".join(new_list)
 
 print(combining(l1,l2))
+
